d2ffd38debdf69fbb63c56bdcbdb0a6b5d2d5956
rjagadeesan-git <rjagadeesan@chn.aithent.com>
2020-07-13 09:48:24 +0530

    committing on 13-JUL-20

--- src/test/java/NostGroup/NostArtifact/TestCases/AllTest.java
+++ src/test/java/NostGroup/NostArtifact/TestCases/AllTest.java
@@ -24,167 +24,167 @@ import NostGroup.NostArtifact.Transfer.TransferToPharmacy;
 
 public class AllTest extends BrowserAct{
 	
-	NostLogin login=new NostLogin();
-	Protocol prot=new Protocol();
-	Pharmacy pharm=new Pharmacy();
-	Supplier supp=new Supplier();
-	Catalog drug=new Catalog();
-	Patient patient=new Patient();
-	Receive rec=new Receive();
-	TransferToPharmacy transGen=new TransferToPharmacy();
-	Dispense disp=new Dispense();
-	PatientReturns patret= new PatientReturns();
-	Destroy destroy=new Destroy();
-	ReturnToSupplier retsupp=new ReturnToSupplier();
-	Monitor monitor=new Monitor();
-	
-	
-	
-//	################################## Nost Login #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=1)
-	public void nostLogin(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		login.nost_Login(data);
-	}
-	
-//	################################## Maintenance #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void addProtocol(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		prot.addProtocol(data);
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=3)
-	public void addPharmacy(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		pharm.addPharmacy(data);
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=4)
-	public void addSupplier(Hashtable<String,String> data) throws IOException, InterruptedException {
-		supp.addSupplier(data);
-	}
-	
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=5)
-	public void addCatalog(Hashtable<String,String> data) throws InterruptedException, IOException {
-		drug.addDrug(data);
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=6)
-	public void addPatient(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		patient.addPatient(data);
-	}
-
-//  ################################## Receive #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void commonReceive(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		rec.receiveCommon(data);
-		rec.receiveSubmitQueue(data);
-//		rec.rec_SubmitQueueAlone();
-	}
-
-//	################################## Transfer #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void commonTransfer(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		transGen.transferCommon(data);
-	}
-	
-	
-//	################################## Dispense #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void patientSearchDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-	disp.dispPatientSearch();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void saveQueueDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-	disp.dispSaveQueue();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void submitDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		disp.dispSubmit();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void LabelGeneDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		disp.dispGenLabel();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void editSubmitDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		disp.dispEditSubmit();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void reprintLabelDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		disp.dispReprintLabel();
-	}
-	
-	
-//	################################## Patient returns #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void addPatientReturns(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		patret.patReturnsAdd();
-	}
-	
-	
-//	################################## Destroy #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void addDestroy(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		destroy.destroyAdd();
-	}
-	
-//	################################## Return to Supplier #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void addPatReturn(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		retsupp.returnSupplierAdd();
-	}
-	
-//	################################## Monitor #########################################
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void addMonitor(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		monitor.monitorAdd();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void downloadReportsMonitor(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		monitor.monitorDownloadReports();
-	}
-	
-	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
-	public void verifyTransMonitor(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
-		monitor.monitorVerifyTrans();
-	}
-	
-//	################################## Adjust modules #########################################
-
-//	################################## Edit Inventory #########################################
-	
-//	################################## Drug. Acc Reports #########################################
-	
-//	################################## Drug Dispensing Report #########################################
-	
-//	################################## CDM #########################################
-	
-//	################################## CPP #########################################
-	
-//	################################## Service Master #########################################
-	
-//	################################## Review Charges #########################################
-	
-//	################################## Create Bills #########################################
-	
-//	################################## User #########################################
-	
-//	################################## Groups #########################################
-	
+//	NostLogin login=new NostLogin();
+//	Protocol prot=new Protocol();
+//	Pharmacy pharm=new Pharmacy();
+//	Supplier supp=new Supplier();
+//	Catalog drug=new Catalog();
+//	Patient patient=new Patient();
+//	Receive rec=new Receive();
+//	TransferToPharmacy transGen=new TransferToPharmacy();
+//	Dispense disp=new Dispense();
+//	PatientReturns patret= new PatientReturns();
+//	Destroy destroy=new Destroy();
+//	ReturnToSupplier retsupp=new ReturnToSupplier();
+//	Monitor monitor=new Monitor();
+//	
+//	
+//	
+////	################################## Nost Login #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=1)
+//	public void nostLogin(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		login.nost_Login(data);
+//	}
+//	
+////	################################## Maintenance #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void addProtocol(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		prot.addProtocol(data);
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=3)
+//	public void addPharmacy(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		pharm.addPharmacy(data);
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=4)
+//	public void addSupplier(Hashtable<String,String> data) throws IOException, InterruptedException {
+//		supp.addSupplier(data);
+//	}
+//	
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=5)
+//	public void addCatalog(Hashtable<String,String> data) throws InterruptedException, IOException {
+//		drug.addDrug(data);
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=6)
+//	public void addPatient(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		patient.addPatient(data);
+//	}
+//
+////  ################################## Receive #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void commonReceive(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		rec.receiveCommon(data);
+//		rec.receiveSubmitQueue(data);
+////		rec.rec_SubmitQueueAlone();
+//	}
+//
+////	################################## Transfer #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void commonTransfer(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		transGen.transferCommon(data);
+//	}
+//	
+//	
+////	################################## Dispense #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void patientSearchDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//	disp.dispPatientSearch();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void saveQueueDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//	disp.dispSaveQueue();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void submitDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		disp.dispSubmit();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void LabelGeneDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		disp.dispGenLabel();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void editSubmitDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		disp.dispEditSubmit();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void reprintLabelDispense(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		disp.dispReprintLabel();
+//	}
+//	
+//	
+////	################################## Patient returns #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void addPatientReturns(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		patret.patReturnsAdd();
+//	}
+//	
+//	
+////	################################## Destroy #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void addDestroy(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		destroy.destroyAdd();
+//	}
+//	
+////	################################## Return to Supplier #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void addPatReturn(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		retsupp.returnSupplierAdd();
+//	}
+//	
+////	################################## Monitor #########################################
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void addMonitor(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		monitor.monitorAdd();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void downloadReportsMonitor(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		monitor.monitorDownloadReports();
+//	}
+//	
+//	@Test(dataProvider="getData",dataProviderClass = CellDataProvider.class,priority=2)
+//	public void verifyTransMonitor(Hashtable<String,String> data) throws IOException, InterruptedException, AWTException {
+//		monitor.monitorVerifyTrans();
+//	}
+//	
+////	################################## Adjust modules #########################################
+//
+////	################################## Edit Inventory #########################################
+//	
+////	################################## Drug. Acc Reports #########################################
+//	
+////	################################## Drug Dispensing Report #########################################
+//	
+////	################################## CDM #########################################
+//	
+////	################################## CPP #########################################
+//	
+////	################################## Service Master #########################################
+//	
+////	################################## Review Charges #########################################
+//	
+////	################################## Create Bills #########################################
+//	
+////	################################## User #########################################
+//	
+////	################################## Groups #########################################
+//	
 	
 }
